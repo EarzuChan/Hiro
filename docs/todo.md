a0296f2e197e9a2887e8a24b10c0df5854d573b1
Yachiyo <yachiyo@tsukuyomi.web>
2026-07-07 14:30:18 +0800

    chore: bump to 1.0.1-alpha

--- docs/todo.md
+++ docs/todo.md
@@ -1,9 +1,11 @@
 # 待办
 
+- Edge2Edge：建议给Examples换用Ax.Activity的（但那个要求Activity用组件活动），现行的太牢了
+- Compose：R和Res，ViewModel、LifeCycle、SavedState
 - Sysbar适配：基本完成，差更好的用户自定义入口
 - 测试液态玻璃和Miuix：液态已跑通，背后是修了Skiko
 - Input生命周期Log
-- Input手感修复：Lazy的滚动有问题，奇怪阻尼/回弹
+- Input手感调优：AI说可接ViewCfg里的参数
 - 更多Input接入（虚拟触控板、键盘）
 - IME与TextField正常性
 - HGP优化：基本OK
--- packages/compose/src/main/kotlin/me/earzuchan/hiro/compose/HiroActivityExtension.kt
+++ packages/compose/src/main/kotlin/me/earzuchan/hiro/compose/HiroActivityExtension.kt
@@ -3,11 +3,16 @@ package me.earzuchan.hiro.compose
 import android.app.Activity
 import androidx.compose.runtime.Composable
 
+// CHECK：参数没有组合上下文（浮木）
 fun Activity.setHiroComposeContent(content: @Composable () -> Unit): HiroComposeView {
-    val view = HiroComposeView(this)
+    // TODO：原有没有顶替复用
 
-    view.setContent(content)
-    setContentView(view)
+    val composeView = HiroComposeView(this)
 
-    return view
+    composeView.setContent(content)
+
+    // CHECK：没有设SavedStateOwner？会不会有问题？
+    setContentView(composeView)
+
+    return composeView
 }
