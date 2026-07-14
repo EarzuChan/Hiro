a3afbc39d03a841c5774062d8e968f2755fadfc5
Yachiyo <yachiyo@tsukuyomi.web>
2026-07-14 15:41:24 +0800

    chore: bump to 1.6.1

--- build-logic/src/main/kotlin/me/earzuchan/hiro/buildlogic/HiroBuildConfig.kt
+++ build-logic/src/main/kotlin/me/earzuchan/hiro/buildlogic/HiroBuildConfig.kt
@@ -2,7 +2,7 @@ package me.earzuchan.hiro.buildlogic
 
 object HiroBuildConfig {
     const val namespace = "me.earzuchan.hiro"
-    const val version = "1.5.3-alpha-cmp1.11.1"
+    const val version = "1.6.1-alpha-cmp1.11.1"
     const val androidCompileSdk = 36
     const val androidMinSdk = 28
 }
